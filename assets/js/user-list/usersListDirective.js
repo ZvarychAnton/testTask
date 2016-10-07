e20a1c4a94118bed79b3bbd348d5eab0fa0a30f2
anton <anton@riflmedia.com>
2016-10-07 16:34:41 +0300

    add search and sorting

--- assets/js/user-list/usersListDirective.js
+++ assets/js/user-list/usersListDirective.js
@@ -1,10 +1,22 @@
 angular.module("testAppModule").directive('userList', function () {
-        return {
-            restrict: 'E',
-            scope: true,
-            link:function($scope,el,attrs){
-                $scope.titles = ["N","Name","Email","Phone","Address","Street","City","State","Zip","Remove","Edit"];
-            },
-          templateUrl: '/assets/js/user-list/user-list.html'
-        }
-    });
+    return {
+        restrict: 'E',
+        scope: true,
+        link:function($scope,el,attrs){
+            $scope.titles = ["N","name","email","telephone","address","street","city","state","zip","remove","edit"];
+            $scope.propertyName = null;
+            $scope.reverse = false;
+
+            $scope.sortBy = function(propertyName) {
+                $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+                $scope.propertyName = propertyName;
+            };
+            $scope.resetSorting = function(){
+                $scope.propertyName = null;
+                $scope.reverse = false
+            }
+
+        },
+        templateUrl: '/assets/js/user-list/user-list.html'
+    }
+});
